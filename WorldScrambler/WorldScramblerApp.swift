8756a98a71e1c96994366753606df5f8960e9c34
Angelo Cammaroto <hi@angelocammaroto.com>
2021-06-05 19:51:36 +0200

    Initial Commit

---
+++ WorldScrambler/WorldScramblerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScramblerApp.swift
+//  WorldScrambler
+//
+//  Created by Angelo Cammaroto on 29/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WorldScramblerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
